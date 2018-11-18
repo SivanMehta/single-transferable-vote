a5e85ba93a7486e4de3164e973debda3f3a151cc
Sivan Mehta <Sivanmehta2@gmail.com>
2018-11-18 14:58:09 -0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Fragment>hwat</Fragment>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+
+// import store from './redux/store';
+import App from './app';
+
+render(<App />, document.getElementById('content'));
